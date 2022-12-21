d50be68f84ec19fe8d239e2f28adc8647dc13ed6
Sammed Singalkar <sammedsingalkar@gmail.comgit confir --global user,name Sammed>
2022-12-21 19:48:11 +0530

    Modification

--- Web Technology (WT)/Assignment - 3/2/password.js
+++ Web Technology (WT)/Assignment - 3/2/password.js
@@ -1,40 +1,13 @@
-let parameters = {
-    count : false
-}
-let strengthBar = document.getElementById("strength-bar");
-let msg = document.getElementById("msg");
-
-function strengthChecker(){
-    let password = document.getElementById("password").value;
-    parameters.count = (password.length > 8)?true:false;
-    let barLength = Object.values(parameters).filter(value=>value);
-
-    console.log(Object.values(parameters), barLength);
-
-    strengthBar.innerHTML = "";
-    for( let i in barLength){
-        let span = document.createElement("span");
-        span.classList.add("strength");
-        strengthBar.appendChild(span);
+function checkPasswordStrength() {
+    const password = document.getElementById('password').value;
+    if (password.length < 8) {
+      document.getElementById('result').innerHTML = 'Password is weak';
+    } else {
+      document.getElementById('result').innerHTML = 'Password is strong';
     }
+  }
 
-    let spanRef = document.getElementsByClassName("strength");
-    for( let i = 0; i < spanRef.length; i++){
-        switch(spanRef.length){
-            case 0:
-                spanRef[i].style.background = "#ff3e36";
-                msg.textContent = "Your password is Weak";
-                break;
-            case 1:
-                spanRef[i].style.background = "#ff691f";
-                msg.textContent = "Your password is Strong";
-                break;
-        }
-    }
-}
-
-
-function toggle(){
+  function toggle(){
     let password = document.getElementById("password");
     let eye = document.getElementById("toggle");
 
--- Web Technology (WT)/Assignment - 3/3/password.js
+++ Web Technology (WT)/Assignment - 3/3/password.js
@@ -1,56 +1,18 @@
-let parameters = {
-    count : false,
-    letters : false,
-    numbers : false,
-    special : false
-}
-let strengthBar = document.getElementById("strength-bar");
-let msg = document.getElementById("msg");
+function validatePassword() {
+    const password = document.getElementById('password').value;
+    const hasNumber = /\d/.test(password);
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const isAtLeast8Chars = password.length >= 8;
 
-function strengthChecker(){
-    let password = document.getElementById("password").value;
-
-    parameters.letters = (/[A-Za-z]+/.test(password))?true:false;
-    parameters.numbers = (/[0-9]+/.test(password))?true:false;
-    parameters.special = (/[!\"$%&/()=?@~`\\.\';:+=^*_-]+/.test(password))?true:false;
-    parameters.count = (password.length > 8)?true:false;
-
-    let barLength = Object.values(parameters).filter(value=>value);
-
-    console.log(Object.values(parameters), barLength);
-
-    strengthBar.innerHTML = "";
-    for( let i in barLength){
-        let span = document.createElement("span");
-        span.classList.add("strength");
-        strengthBar.appendChild(span);
+    if (hasNumber && hasUppercase && hasLowercase && isAtLeast8Chars) {
+      document.getElementById('result').innerHTML = 'Password is strong';
+    } else {
+      document.getElementById('result').innerHTML = 'Password is weak';
     }
+  }
 
-    let spanRef = document.getElementsByClassName("strength");
-    for( let i = 0; i < spanRef.length; i++){
-        switch(spanRef.length - 1){
-            case 0 :
-                spanRef[i].style.background = "#ff3e36";
-                msg.textContent = "Your password is very weak";
-                break;
-            case 1:
-                spanRef[i].style.background = "#ff691f";
-                msg.textContent = "Your password is weak";
-                break;
-            case 2:
-                spanRef[i].style.background = "#ffda36";
-                msg.textContent = "Your password is good";
-                break;
-            case 3:
-                spanRef[i].style.background = "#0be881";
-                msg.textContent = "Your password is strong";
-                break;
-        }
-    }
-}
-
-
-function toggle(){
+  function toggle(){
     let password = document.getElementById("password");
     let eye = document.getElementById("toggle");
 
